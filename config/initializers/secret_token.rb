f09ded372d4a46cd94c247f6a3ac57ec95ee585b
Oleg <kalinon7@gmail.com>
2013-06-04 08:32:02 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = '6717a3979568497c3db6400e9122642b78a1491b99fd4e8d31c6022c95d67543bebcf4498977887cb6698e2b4a2bfe9b8f0ce2c6c619a605b153fcb557e95fec'
---
+++ db/migrate/20130603192441_combine_items_in_cart.rb
@@ -0,0 +1,19 @@
+class CombineItemsInCart < ActiveRecord::Migration
+  def up
+    # замена нескольких записей для одного и того же товара в корзине одной записью
+        Cart.all.each do |cart|
+            # подсчет количества каждого товара в корзине
+            sums = cart.line_items.group(:product_id).sum(:quantity)
+            sums.each do |product_id, quantity|
+                if quantity > 1
+                    # удаление отдельных записей
+                    cart.line_items.where(product_id: product_id).delete_all
+                    # замена одной записью
+                    cart.line_items.create(product_id: product_id, quantity: quantity)
+                end
+    end end
+  end
+
+  def down
+  end
+end
