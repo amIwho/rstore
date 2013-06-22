35c39a563e573aa1957325aa18ca222f860a5614
root <root@altaviel.ru>
2013-06-22 21:41:02 +0400

    mail send ready

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,7 +1,7 @@
-ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.raise_delivery_errors = true
 ActionMailer::Base.smtp_settings = {
-    address:              "smtp.altaviel.ru",
+    address:              "mail.altaviel.ru",
     port:                 "25",
     domain:               "altaviel.ru",
     user_name:            "noreply@altaviel.ru",
@@ -9,4 +9,9 @@ ActionMailer::Base.smtp_settings = {
     openssl_verify_mode:  'none',
     authentication:       :plain,
     enable_starttls_auto: true
+}
+
+ActionMailer::Base.sendmail_settings = {
+:location       => '/usr/sbin/sendmail',
+:arguments      => '-i -t'
 }
